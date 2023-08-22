30e28ff171dcc617fe2cc2774163c5f8e961c8a9
jackmcg27 <jack.mcgowan2001@gmail.com>
2023-08-22 19:43:46 -0400

    Initial commit

--- Lab01/Hello.java
+++ Lab01/Hello.java
@@ -1,1 +1,18 @@
+/*
+* Hello.java
+*
+* Jack McGowan
+* 8/22/2023
+* CMSC255 C90
+ */
 
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Jack McGowan");
+        System.out.println("Hello.java");
+        System.out.println("Intro program for Lab 01");
+        System.out.println("8/22/23");
+        System.out.println("CMSC255 C90");
+        System.out.println();
+    }
+}
